db17b2e8e0a9d33947685e07fa3ddeee6e6cc887
MArkDArks <medlisuport70@gmail.com>
2019-05-21 11:31:28 -0500

    firstcommit

---
+++ app/src/main/java/com/example/medliv_10/ActMetodopago.java
@@ -0,0 +1,13 @@
+package com.example.medliv_10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActMetodopago extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_metodopago);
+    }
+}
